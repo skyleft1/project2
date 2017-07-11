58f67cdf445f0f9ca7bf47e315ad2f8f89b211f7
skyleft1 <skyleft@naver.com>
2017-07-11 12:38:05 +0900

    1

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -4,27 +4,23 @@ $(document).ready(function(){
 		$('.header').load('guide/header.jsp');
 		$('.footer').load('guide/footer.jsp');
 	});
-});
 	
-$(document).ready(function(){
 	$(document).on('mouseenter', '.warp_menu_inner', function(e){
 		$('.wrap_inner').slideDown(200);
 	})
 	$(document).on('mouseleave', '.warp_menu_inner', function(e){
 		$('.wrap_inner').slideUp(200);
 	});
-});
 	
-$(document).ready(function(){
+
 	$(document).on('mouseenter', '.menu_content li h3, .menu_content_inner li h5', function(e){
 		$(this).css('color', '#378c42 !important');
 	});
 	$(document).on('mouseleave', '.menu_content li h3, .menu_content_inner li h5', function(e){
 		$(this).css('color', '');
 	});
-});
 
-$(document).ready(function(){
+
 	$(function(){
 		$.datepicker.setDefaults($.datepicker.regional['ko']); 
 		$( ".calander1" ).datepicker({
